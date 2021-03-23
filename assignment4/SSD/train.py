a2468a0e0f07ca5a998a4568c8912f7f8f892a7c
Agnes Marie Ødegård Ockernahl <amockern@oppdal.idi.ntnu.no>
2021-03-23 17:25:53 +0100

    økte threshold og strengere weight-decay (L2 regularization), Adam optimizer

--- assignment4/SSD/train.py
+++ assignment4/SSD/train.py
@@ -35,10 +35,17 @@ def start_train(cfg):
     model = SSDDetector(cfg)
     model = torch_utils.to_cuda(model)
 
+    """
     optimizer = torch.optim.SGD(
         model.parameters(),
         lr=cfg.SOLVER.LR,
         momentum=cfg.SOLVER.MOMENTUM,
+        weight_decay=cfg.SOLVER.WEIGHT_DECAY
+    )"""
+    
+    optimizer = torch.optim.Adam(
+        model.parameters(),
+        lr=cfg.SOLVER.LR,
         weight_decay=cfg.SOLVER.WEIGHT_DECAY
     )
 
